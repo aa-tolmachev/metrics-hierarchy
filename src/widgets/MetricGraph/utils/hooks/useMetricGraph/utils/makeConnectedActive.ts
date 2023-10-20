210f0514916e328464b7b638370f7e936988a904
Maksimov Vladislav <workmaximov@yandex.ru>
2023-10-20 03:02:59 +0200

    fix edge click logic

--- src/widgets/MetricGraph/utils/hooks/useMetricGraph/utils/makeConnectedActive.ts
+++ src/widgets/MetricGraph/utils/hooks/useMetricGraph/utils/makeConnectedActive.ts
@@ -3,6 +3,7 @@ import { Item } from "@antv/g6";
 export const makeConnectedActive = (edge: Item) => {
   const source = edge.get<Item>("source");
   const target = edge.get<Item>("target");
+  edge.setState("active", true);
   source.setState("active", true);
   target.setState("active", true);
 };
